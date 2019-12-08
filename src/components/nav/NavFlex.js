b3ee1286496c4bb49489e7d8fc4d9e85ab123b98
Matthew J. Hughes <mjhughes707@gmail.com>
2019-12-07 16:14:36 -0800

    added a home link to nav bar

--- src/components/nav/NavFlex.js
+++ src/components/nav/NavFlex.js
@@ -2,13 +2,11 @@ import React from "react"
 
 import NavLink from "./NavLink"
 
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faHome } from '@fortawesome/free-solid-svg-icons'
-
 function NavFlex() {
   return (
     <nav className="nav-flex">
       <ul>
+        <NavLink link="#home" title="HOME" />
         <NavLink link="#about" title="ABOUT" />
         <NavLink link="#projects" title="PROJECTS" />
         <NavLink link="#blog" title="BLOG" />
