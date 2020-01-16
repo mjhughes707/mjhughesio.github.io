75ec3d626d49140b01338bc480884497d7932705
Matthew J. Hughes <mjhughes707@gmail.com>
2020-01-16 12:27:03 -0800

    updated projects + styling

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,22 +1,36 @@
-import React from "react"
+import React from "react";
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDesktop } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-function Project(props) {
+function Project({ id, image, alt, overlay, name, link, code }) {
   return (
-    <div id={props.id} className="project-container">
-      <img
-        src={props.image}
-        alt={props.alt}
-        className="project-image"
-      />
-      <div className={props.overlay}>
+    <div id={id} className="project-container">
+      <img src={image} alt={alt} className="project-image" />
+      <div className={overlay}>
         <div className="project-text">
-          <h5>{props.name}</h5>
-          <a href={props.link}>View Live</a>
+          <h5>{name}</h5>
+          <a href={link}>
+            <FontAwesomeIcon icon={faDesktop} />
+          </a>
+          <a href={code}>
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
+Project.propTypes = {
+  id: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  overlay: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  code: PropTypes.string.isRequired,
+};
 
-export default Project
+export default Project;
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import Project from "./Project";
 
-import BouncingCube from "../../img/bouncing-cube.png";
+import PodGuild from "../../img/pod-guild.png";
 import FoodCartCity from "../../img/foodcartcity.png";
-import MakeAMeowm from "../../img/make-a-meowm.png";
+import StatesGame from "../../img/states-game.png";
 import Stocker from "../../img/stocker.png";
+import MakeAMeowm from "../../img/make-a-meowm.png";
+import BouncingCube from "../../img/bouncing-cube.png";
 
 function Projects() {
   return (
@@ -15,27 +17,30 @@ function Projects() {
       <div className="project-grid">
         <Project
           id="project1"
-          image={FoodCartCity}
-          alt="Preview of the Food Cart City project"
+          image={PodGuild}
+          alt="Preview of the Pod Guild project"
           overlay="project-overlay-a"
-          name="Food Cart City"
-          link="https://foodcartcity.pythonanywhere.com"
+          name="Pod Guild"
+          link="https://matthewjhughes.com/"
+          code="https://github.com/mjhughesio/podguild"
         />
         <Project
           id="project2"
-          image={MakeAMeowm}
-          alt="Preview of the Make-A-Meowm project"
+          image={FoodCartCity}
+          alt="Preview of the Food Cart City project"
           overlay="project-overlay-b"
-          name="Make-A-Meowm"
-          link="https://matthewjhughes.com/make-a-meowm"
+          name="Food Cart City"
+          link="https://foodcartcity.pythonanywhere.com"
+          code="https://github.com/mjhughesio/foodcartcity"
         />
         <Project
           id="project3"
-          image={BouncingCube}
-          alt="Preview of The Office - Bouncing Cube project"
+          image={StatesGame}
+          alt="Preview of Chandler's Dumb States Game"
           overlay="project-overlay-a"
-          name="Bouncing Cube"
-          link="https://matthewjhughes.com/bouncing-cube"
+          name="States Game"
+          link="https://matthewjhughes.com/states-game"
+          code="https://github.com/mjhughesio/states-game"
         />
         <Project
           id="project4"
@@ -44,6 +49,25 @@ function Projects() {
           overlay="project-overlay-b"
           name="Stocker"
           link="https://matthewjhughes.com/stocker"
+          code="https://github.com/mjhughesio/stocker"
+        />
+        <Project
+          id="project5"
+          image={MakeAMeowm}
+          alt="Preview of the Make-A-Meowm project"
+          overlay="project-overlay-a"
+          name="Make-A-Meowm"
+          link="https://matthewjhughes.com/make-a-meowm"
+          code="https://github.com/mjhughesio/make-a-meowm"
+        />
+        <Project
+          id="project6"
+          image={BouncingCube}
+          alt="Preview of The Office - Bouncing Cube project"
+          overlay="project-overlay-b"
+          name="Bouncing Cube"
+          link="https://matthewjhughes.com/bouncing-cube"
+          code="https://github.com/mjhughesio/bouncing-cube"
         />
       </div>
     </div>
