907346cd2e9aebacdf9610bc4ff34ea7614c632f
Matthew J. Hughes <mjhughes707@gmail.com>
2020-01-16 14:13:36 -0800

    updated contact form submission handling - temp fix

--- src/components/contact/Form.js
+++ src/components/contact/Form.js
@@ -1,26 +1,71 @@
-import React from "react"
+import React, { useState } from "react";
 
 function Form() {
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+  const { name, email, phone, message } = userData;
+
+  const handleChange = e =>
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+
   return (
-      <form action="http://webhook.site/e29ebfec-9ba2-4af7-bd95-9351adf4ace6" method="post">
-        <div className="form-field">
-          <input type="text" name="name" id="name" placeholder="Name" minLength="2" title="Requires at least two characters" required />
-        </div>
-        <div className="form-field">
-          <input type="email" name="email" id="email" placeholder="Email" required />
-        </div>
-        <div className="form-field">
-          <input type="tel" name="phone" id="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="Phone #" title="Example: 555-555-5555" />
-        </div>
-        <div className="form-field">
-          <textarea name="message" id="message" placeholder="Message" required ></textarea>
-        </div>
-        <div className="form-field submit-div">
-          <button className="submit">Submit</button>
-        </div>
-      </form>
-  )
+    <form action="https://formspree.io/mknjvjqe" method="POST">
+      <div className="form-field">
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={e => handleChange(e)}
+          id="name"
+          placeholder="Name"
+          minLength="2"
+          title="Requires at least two characters"
+          required
+        />
+      </div>
+      <div className="form-field">
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={e => handleChange(e)}
+          id="email"
+          placeholder="Email"
+          required
+        />
+      </div>
+      <div className="form-field">
+        <input
+          type="tel"
+          name="phone"
+          value={phone}
+          onChange={e => handleChange(e)}
+          id="phone"
+          pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+          placeholder="Phone #"
+          title="Example: 555-555-5555"
+        />
+      </div>
+      <div className="form-field">
+        <textarea
+          name="message"
+          value={message}
+          onChange={e => handleChange(e)}
+          id="message"
+          placeholder="Message"
+          required
+        ></textarea>
+      </div>
+      <div className="form-field submit-div">
+        <button className="submit">Submit</button>
+      </div>
+    </form>
+  );
 }
 
-
-export default Form
+export default Form;
